9f283dedd8195a6cb096bfc46cabf2f9a8d1acb1
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2018-12-01 12:51:30 +0000

    first exercise done

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,13 +1,15 @@
 import Typed from 'typed.js';
 
 const typedText = function() {
-    const element = document.getElementById("#js-typed-text");
-    if(element) {
+    const element = document.getElementById("js-typed-text");
+    console.log(element)
+    if (element) {
+        console.log("inside if condition")
             new Typed('#js-typed-text', {
             strings: ["ruby", "react", "elixir", "python", "swift"],
             typeSpeed: 70,
             loop: true
-        }
+        });
     }
 }
 
