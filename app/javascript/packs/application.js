6e78a4730ace4dd509e0c527d34a5661b68a9710
Rui Freitas <dinnerpartysaboteur@gmail.com>
2018-12-01 12:38:32 +0000

    removed prettier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,1 @@
 import 'bootstrap';
-import './application.css';
-import { loadDynamicBannerText } from '../components/banner.js';
-
-
-const homePage = document.querySelector('.pages.home');
-if (homePage) {
-  loadDynamicBannerText();
-}
